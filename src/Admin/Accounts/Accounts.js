a1fa69b13ab76b7e5bb8be2fb443f83a8b8bb346
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2023-10-13 15:40:30 +0100

    manage account

--- src/Admin/Accounts/Accounts.js
+++ src/Admin/Accounts/Accounts.js
@@ -1,9 +1,159 @@
-import AdminLayout from "../../Pages/AdminLayout";
-function Accounts() {
-  const AccountsContent = <div>Accounts</div>;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import AdminLayout from '../../Pages/AdminLayout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash,faPen,faEye,faXmark } from '@fortawesome/free-solid-svg-icons';
+
+function UserAccountManager({ user, onUpdate, onDelete }) {
+  const [updating, setUpdating] = useState(false);
+  const [updatedUser, setUpdatedUser] = useState({ ...user });
+
+  const handleUpdate = () => {
+    // Make an API call to update the user data
+    axios.put(`http://your-api-url/users/${user.id}`, updatedUser) // Replace with your actual API endpoint
+      .then(response => {
+        onUpdate(response.data);
+        setUpdating(false);
+      })
+      .catch(error => {
+        console.error('Error updating user data:', error);
+        setUpdating(false);
+      });
+  };
+
+  const handleDelete = () => {
+    // Make an API call to delete the user account
+    axios.delete(`http://your-api-url/users/${user.id}`) // Replace with your actual API endpoint
+      .then(() => {
+        onDelete(user.id);
+        console.log('User account deleted successfully');
+      })
+      .catch(error => {
+        console.error('Error deleting user account:', error);
+      });
+  };
 
   return (
-    <AdminLayout Content={AccountsContent} />
+    <div className="">
+      {/* <h1 className="text-2xl font-bold mb-4">User Account</h1> */}
+
+      <div className="flex shadow-md flex-row space-x-8 items-center p-2 mb-2 justify-center gap-9 rounded-lg ml-10 bg-white">
+        {/* <div className="flex-none w-10">
+          <img className="" alt="img" src="" />
+        </div> */}
+        <div className="flex-grow w-40">
+          <div className="truncate border">{user.name}</div>
+        </div>
+        <div className="flex-none w-25">
+          <div>{user.email}</div>
+        </div>
+        {/* <div className="flex-none w-25">
+          <div>{user.email_verified_at!==null?user.email_verified_at:<FontAwesomeIcon className='text-red-600' icon={faXmark} />}</div>
+        </div> */}
+        <div className="flex-none w-25">
+          <div>{user.created_at}</div>
+        </div>
+        <div className="flex-none">
+          <FontAwesomeIcon className="text-indigo-500" icon={faEye} />
+        </div>
+        <div className="flex-none">
+          <FontAwesomeIcon onClick={handleDelete} className="text-indigo-500 hover:cursor-pointer" icon={faTrash} />
+        </div>
+        <div className="flex-none">
+          <FontAwesomeIcon onClick={() => setUpdating(true)} className="text-indigo-500" icon={faPen} />
+        </div>
+      </div>
+
+
+      {updating && (
+        <div>
+          <h2 className="text-lg font-semibold mb-2">Update User</h2>
+          <input
+            type="text"
+            placeholder="New Username"
+            value={updatedUser.username}
+            onChange={(e) => setUpdatedUser({ ...updatedUser, username: e.target.value })}
+            className="w-full p-2 mb-2 border border-gray-300 rounded"
+          />
+          <input
+            type="text"
+            placeholder="New Email"
+            value={updatedUser.email}
+            onChange={(e) => setUpdatedUser({ ...updatedUser, email: e.target.value })}
+            className="w-full p-2 mb-2 border border-gray-300 rounded"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleUpdate}
+          >
+            Save
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
+
+function Accounts() {
+  const [usersData, setUsersData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch user data from the backend when the component mounts
+    axios.get('http://127.0.0.1:8000/api/Users') // Replace with your actual API endpoint
+      .then(response => {
+        setUsersData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  const updateUser = (updatedUser) => {
+    const updatedUsers = usersData.map(user => (user.id === updatedUser.id ? updatedUser : user));
+    setUsersData(updatedUsers);
+  };
+
+  const deleteUser = (userId) => {
+    const updatedUsers = usersData.filter(user => user.id !== userId);
+    setUsersData(updatedUsers);
+  };
+
+  return (
+    <AdminLayout>
+      <div className="container mx-auto mt-8 px-10 bg-white ml-5 pb-4">
+        <div className="max-w-screen-lg">
+          <h1 className="text-3xl font-bold mb-4">User Accounts</h1>
+          {loading ? (
+            <p>Loading user data...</p>
+          ) : (
+            usersData.map(user => (
+              <UserAccountManager
+                key={user.id}
+                user={user}
+                onUpdate={updateUser}
+                onDelete={deleteUser}
+              />
+            ))
+          )}
+        </div>
+      </div>
+    </AdminLayout>
+  );
+}
+
+
+
+
+
+
+/* function Accounts() {
+  //const AccountsContent = <div className="container mx-auto mt-8 px-10 bg-white ml-5 " ><div className="max-w-screen-lg"></div>Accounts</div>;
+
+  return (
+    <AdminLayout Content={<UserAccountManager/>} />
+  );
+} */
 export default Accounts;
--- src/Admin/Posts/ViewPost.js
+++ src/Admin/Posts/ViewPost.js
@@ -2,6 +2,9 @@ import { useParams } from "react-router-dom";
 import AdminLayout from "../../Pages/AdminLayout";
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
 
 function View() {
   // Get the value of the "id" parameter from the URL
@@ -32,6 +35,65 @@ function Getpost({ postId }) {
   }, [postId]); // Include postId in the dependency array
 
   // Conditionally render content based on loading state
+  //Delete a comment relationale to a post
+  let comment_body = ""; // Define variables to hold comment details
+  let comment_username = "";
+  let created_at = "";
+  
+  const handeleDeleteComment = (id) => {
+    console.log("commentid", id);
+  
+    // Find the comment to delete
+    const commentToDelete = postData.comments.find((comment) => comment.id === id);
+  
+    if (commentToDelete) {
+      const comment_body = commentToDelete.body;
+      const comment_username = commentToDelete.username;
+      const created_at = commentToDelete.created_at;
+  
+      // Display a confirmation dialog using Swal
+      Swal.fire({
+        title: 'Delete Comment',
+        text: 'Are you sure you want to delete this comment?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        cancelButtonText: 'Cancel',
+        html: `
+          <p><strong>Username:</strong> ${comment_username}</p>
+          <p><strong>Comment Body:</strong> ${comment_body}</p>
+          <p><strong>Created At:</strong> ${created_at}</p>
+        `,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Make an API call to delete the comment here
+          axios
+            .delete(`http://127.0.0.1:8000/api/Comment/${id}`)
+            .then((response) => {
+              if (response.status === 200) {
+                // Create a new object with updated comments by filtering out the deleted comment
+                const updatedComments = postData.comments.filter((comment) => comment.id !== id);
+                
+                // Update the state with the new object
+                setPostData({ ...postData, comments: updatedComments });
+  
+                // Display a success message
+                Swal.fire('Comment Deleted', 'The comment has been deleted.', 'success');
+              } else {
+                throw new Error(response.data.message.error);
+              }
+            })
+            .catch((error) => {
+              Swal.fire('Error', error.response.data.error || error.response.data.message, 'error');
+            });
+        }
+      });
+    }
+  };
+  
+  
+  
+  
   return (
     <div className=" shadow-md flex-row px-1  items-center mt-5 pl-5 pt-2 pb-2 mb-2 justify-center  rounded-lg ml-10 bg-white">
       {loading ? (
@@ -75,6 +137,7 @@ function Getpost({ postId }) {
                 <th className="py-2 px-3 bg-gray-200 font-semibold">Content</th>
                 <th className="py-2 px-3 bg-gray-200 font-semibold">Created at</th>
                 <th className="py-2 px-3 bg-gray-200 font-semibold">Updated at</th>
+                <th className="py-2 px-3 bg-gray-200 font-semibold">Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -84,6 +147,7 @@ function Getpost({ postId }) {
                   <td className="py-2 px-3">{comment.body}</td>
                   <td className="py-2 px-3">{comment.created_at}</td>
                   <td className="py-2 px-3">{comment.updated_at}</td>
+                  <td className="py-2 px-3 text-indigo-500 "><FontAwesomeIcon className="hover:cursor-pointer" onClick={()=>handeleDeleteComment(comment.id)} icon={faTrash} /></td>
                 </tr>
               ))}
             </tbody>
