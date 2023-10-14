65dc5dc93dc1e2c81a1ee68049bdb4d76a8284a2
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2023-10-14 14:54:01 +0100

    update acc info

--- src/Admin/Accounts/Accounts.js
+++ src/Admin/Accounts/Accounts.js
@@ -11,7 +11,7 @@ function UserAccountManager({ user, onUpdate, onDelete }) {
 
   const handleUpdate = () => {
     // Make an API call to update the user data
-    axios
+    /* axios
       .put(`http://127.0.0.1:8000/Users/${user.id}`, updatedUser) // Replace with your actual API endpoint
       .then(response => {
         onUpdate(response.data);
@@ -20,9 +20,48 @@ function UserAccountManager({ user, onUpdate, onDelete }) {
       .catch(error => {
         console.error('Error updating user data:', error);
         setUpdating(false);
-      });
-  };
+      }); */
 
+      Swal.fire({
+        title: 'Are you sure you want to update this user information account?',
+        icon: 'warning',
+        html: `
+          <p><b>Username</b>: ${updatedUser.name}</p>
+          <p><b>Email</b>: ${updatedUser.email}</p>
+        `,
+        showCancelButton: true,
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          return axios
+            .put(`http://127.0.0.1:8000/api/Users/${user.id}`,updatedUser)
+            .then((response) => {
+              if (response.status === 200) {
+                onUpdate(response.data);
+                setUpdating(false);
+                return response.data;
+              } else {
+                throw new Error(response.data.message.error);
+              }
+            })
+            .catch((error) => {
+              Swal.showValidationMessage(error.response.data.error ? error.response.data.error : error.response.data.message);
+            });
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          onUpdate(updatedUser);
+          setUpdating(false);
+          Swal.fire('User updated', 'The account has been modify.', 'success');
+        }
+      });
+
+  };
+  
+  const handleClose=()=>{
+    setUpdating(false);
+  }
   const handleDelete = () => {
     Swal.fire({
       title: 'Are you sure you want to delete this user account?',
@@ -73,7 +112,7 @@ function UserAccountManager({ user, onUpdate, onDelete }) {
           <FontAwesomeIcon onClick={handleDelete} className="text-indigo-500 hover:cursor-pointer" icon={faTrash} />
         </td>
         <td className="border p-2">
-          <FontAwesomeIcon onClick={() => setUpdating(true)} className="text-indigo-500" icon={faPen} />
+          <FontAwesomeIcon onClick={() => setUpdating(true)} className="text-indigo-500 hover:cursor-pointer" icon={faPen} />
         </td>
       </tr>
 
@@ -83,8 +122,8 @@ function UserAccountManager({ user, onUpdate, onDelete }) {
           <input
             type="text"
             placeholder="New Username"
-            value={updatedUser.username}
-            onChange={(e) => setUpdatedUser({ ...updatedUser, username: e.target.value })}
+            value={updatedUser.name}
+            onChange={(e) => setUpdatedUser({ ...updatedUser, name: e.target.value })}
             className="w-full p-2 mb-2 border border-gray-300 rounded"
           />
           <input
@@ -99,6 +138,10 @@ function UserAccountManager({ user, onUpdate, onDelete }) {
             onClick={handleUpdate}
           >
             Save
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-3 mb-2 rounded"
+            onClick={handleClose}>
+              Close
           </button>
         </div>
       )}
